d7d6ab822b29aca1b5f1485091284df14702e70d
nyxsraid <sahrulramdan.75@gmail.com>
2022-12-15 19:28:07 +0700

    Adding Overlay for Model Images

--- src/components/Card/CardModel.jsx
+++ src/components/Card/CardModel.jsx
@@ -5,15 +5,15 @@ import 'react-lazy-load-image-component/src/effects/blur.css';
 function CardModel(props) {
   return (
     <div className="w-[12.375rem] h-[15.25rem] relative">
-      <div className="bg-black/20 absolute w-full h-full object-cover z-0" />
       <LazyLoadImage
         width={198} height={244}
         placeholderSrc={props.placeholder}
         effect="blur"
         src={props.foto}
-        className="w-full h-full object-cover rounded-2xl"
+        className="w-full h-full object-cover rounded-2xl z-0"
         alt={props.text}
       />
+      <div className="bg-black/20 h-full w-full absolute top-0"/>
       <p className="absolute bottom-5 left-3 z-10 text-white font-bold text-xl">
         {props.text}
       </p>
